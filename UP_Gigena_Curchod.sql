5094d76b3b2daecdf885154a8f22988e27d5699a
Martin Gigena Curchod <magc13@hotmail.com>
2023-02-21 14:14:31 -0300

    Primer entrega

---
+++ UP_Gigena_Curchod.sql
@@ -0,0 +1,56 @@
+CREATE SCHEMA universidad_privada;
+USE universidad_privada;
+
+CREATE TABLE personas(
+dni INT NOT NULL PRIMARY KEY,
+nombre varchar (50) NOT NULL,
+apellido varchar (50) NOT NULL,
+email varchar (50) NOT NULL,
+domicilio varchar (50) NOT NULL,
+telefono INT NOT NULL,
+fecha_de_nacimiento date NOT NULL);
+
+CREATE TABLE rol(
+rolID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+nombre SET ('Autoridad', 'Docente', 'No docente', 'alumno', 'proveedor') NOT NULL);
+
+CREATE TABLE facultades(
+facultadID INT NOT NULL PRIMARY KEY,
+nombre varchar (50) NOT NULL,
+sigla varchar (50) NOT NULL,
+fecha_de_creacion date NOT NULL,
+personaID INT NOT NULL,
+rolID INT NOT NULL,
+foreign key (personaID) REFERENCES personas(dni),
+foreign key (rolID) REFERENCES rol(rolID));
+
+CREATE TABLE carreras(
+carreraID INT NOT NULL PRIMARY KEY,
+facultadID INT NOT NULL,
+nombre varchar (50) NOT NULL,
+sigla varchar (50) NOT NULL,
+grado SET ('pregado', 'grado', 'posgrado', 'diplomatura', 'cursos cortos') NOT NULL,
+fecha_de_creacion date NOT NULL,
+duracion tinyint NOT NULL,
+foreign key (facultadID) REFERENCES facultades(facultadID));
+
+CREATE TABLE anio_cursado(
+anio_cursado INT NOT NULL PRIMARY KEY,
+nombre varchar (50) NOT NULL);
+
+CREATE TABLE materias(
+materiaID INT NOT NULL PRIMARY KEY,
+nombre varchar (50) NOT NULL,
+anio_cursado INT NOT NULL,
+tipo_duracion SET ('anual', 'primer semestre', 'segundo semestre') NOT NULL,
+foreign key (anio_cursado) REFERENCES anio_cursado(anio_cursado));
+
+CREATE TABLE inscripciones(
+inscripcionID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+personaID INT NOT NULL,
+rolID INT NOT NULL,
+materiaID INT NOT NULL,
+fecha_hora_inscripcion datetime NOT NULL,
+foreign key (personaID) REFERENCES personas(dni),
+foreign key (rolID) REFERENCES rol(rolID),
+foreign key (materiaID) REFERENCES materias (materiaID));
